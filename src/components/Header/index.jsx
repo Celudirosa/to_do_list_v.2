9e75b58f3da8319a52de10f8c9b7daa61b1e15a6
Celudirosa <cluqued@live.com>
2023-05-10 12:16:02 +0200

    🌱 initial commit

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import styles from './header.module.css'
+import { BsFillPatchPlusFill } from 'react-icons/bs'
+
+export function Header({ onAddTask }) {
+
+  const [title, setTitle] = useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    onAddTask(title);
+    setTitle('');
+  }
+
+  function onChangeTitle(event) {
+    setTitle(event.target.value);
+  }
+
+  return (
+    <header className={styles.header}>
+      <h1>To do:</h1>
+
+      <form onSubmit={handleSubmit} className={styles.newTaskForm}>
+        <input placeholder='Add a new task' type='text' value={title} onChange={onChangeTitle} />
+        <button>Add
+          <BsFillPatchPlusFill size={20} />
+        </button>
+      </form>
+    </header>
+  )
+}
